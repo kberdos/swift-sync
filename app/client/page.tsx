7f28f4abc6888fe67badb05fca73e0fcf89d5961
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-06-30 23:21:34 -0400

    feat. calendars

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -4,6 +4,22 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Home() {
   const { user, isLoading } = useUser()
 
+  async function handleTestUser() {
+    if (!user) {
+      return
+    }
+
+    const res = await fetch(process.env.NEXT_PUBLIC_backendDomain + "/client/user",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          subclaim: user.sub
+        })
+      })
+
+  }
+
   if (isLoading) {
     return (
       <div>Loading...</div>
@@ -21,6 +37,9 @@ export default function Home() {
   return (
     user && (
       <div>
+        <div>
+          <button onClick={handleTestUser}> Test User</button>
+        </div>
         <a href="/api/auth/logout">Log Out</a>
         <div>{user.name}</div>
         <img src={user.picture!} alt={user.name!} />
